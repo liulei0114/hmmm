775d99efb0c601d151f7814182e6a0035e89f442
liulei <443487268@qq.com>
2021-02-08 12:21:50 +0800

    河马面面初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import _ from 'lodash'
+import md5 from 'js-md5'
+// 1.导入 monment 文件
+import moment from 'moment'
+
+// 通用css normal.css
+import 'normalize.css/normalize.css'
+import '@/style/index.css'
+
+import '@/icons/index'
 
 Vue.config.productionTip = false
+Vue.prototype._ = _
+Vue.prototype.$bus = new Vue()
+Vue.prototype.$md5 = md5
+Vue.prototype.$moment = moment
 
+Vue.use(ElementUI)
+
+// 3.调用API进行汉化
+moment.locale('zh-cn')
 new Vue({
   router,
   store,
